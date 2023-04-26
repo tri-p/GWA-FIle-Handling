85a8641df9e9cb1d607bc1374aaff43823e50484
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-26 18:06:55 +0800

    Set the lowest possible GWA and a GWA that is invalid

--- gwa file handling.py
+++ gwa file handling.py
@@ -4,10 +4,16 @@
 
 # Open students_gwa.txt (read)
 with open("students_gwa.txt") as input_students_gwa:
+    # Set the lowest possible GWA a student can have and set a GWA that is invalid
+    highest_gwa = 5.00
+    lowest_gwa = 0.99
+    highest_student = ""
+    highest_student_gwa = ""
     # Read the inputs for students and GWA line by line and split the name and their GWA
     for line in input_students_gwa:
         student, gwa_str = line.strip().split(": ")
-
+        # If the GWA < lowest possible GWA and > invalid GWA, get highest GWA and name
+        # If there is more than one student who got the highest GWA, record it as well
 
 
 # Print the output
