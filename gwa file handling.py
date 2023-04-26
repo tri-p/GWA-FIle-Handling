0458b1b554ca9e75663c81469eac6152c7a4a704
Katrina Pabuna <kinapabuna@gmail.com>
2023-04-26 18:00:24 +0800

    Open a text file that contains 20 names with GWA

---
+++ gwa file handling.py
@@ -0,0 +1,11 @@
+# PABUNA, KATRINA B. 
+# Write a Python program that read a file containing the name of 20 students together with their GWA
+# The program will output the name of the student who got the highest GWA (including the GWA)
+
+# Open students_gwa.txt (read)
+with open("students_gwa.txt") as input_students_gwa:
+ 
+
+
+
+# Print the output
